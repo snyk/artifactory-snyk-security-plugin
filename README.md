c131cae5ee5af2b9a9dd4c255a9291ee8f52fead
pavel <36902936+pavel-github@users.noreply.github.com>
2019-05-23 23:47:37 +0200

    Initial commit
    squashed commit from 'retrofit2' branch

---
+++ README.md
@@ -0,0 +1,1 @@
+# Artifactory Snyk Security Plugin
---
+++ snyk-sdk/src/main/java/io/snyk/sdk/api/v1/SnykClient.java
@@ -0,0 +1,27 @@
+package io.snyk.sdk.api.v1;
+
+import io.snyk.sdk.model.TestResult;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface SnykClient {
+
+  /**
+   * Test Maven packages for issues according to their coordinates: group ID, artifact ID and version:
+   * <ul>
+   * <li><strong>groupId</strong></li>
+   * <li><strong>artifactId</strong></li>
+   * <li><strong>version</strong></li>
+   * <li>organisation (optional)</li>
+   * <li>repository (optional)</li>
+   * </ul>
+   */
+  @GET("test/maven/{groupId}/{artifactId}/{version}")
+  Call<TestResult> testMavenByGAV(@Path("groupId") String groupId,
+                                  @Path("artifactId") String artifactId,
+                                  @Path("version") String version,
+                                  @Query("org") String organisation,
+                                  @Query("repository") String repository);
+}

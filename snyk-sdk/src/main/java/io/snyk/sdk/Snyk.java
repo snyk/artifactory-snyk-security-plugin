144a65aca1c9cb262b419bfcfc605045fda41dd4
pavel <36902936+pavel-github@users.noreply.github.com>
2019-05-23 23:49:28 +0200

    Add client builder with service interceptor

---
+++ snyk-sdk/src/main/java/io/snyk/sdk/Snyk.java
@@ -0,0 +1,67 @@
+package io.snyk.sdk;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.snyk.sdk.api.v1.SnykClient;
+import io.snyk.sdk.interceptor.ServiceInterceptor;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+public class Snyk {
+
+  private static final String DEFAULT_BASE_URL = "https://snyk.io/api/v1/";
+  private static final String DEFAULT_USER_AGENT = "snyk-sdk-java";
+  private static final long DEFAULT_CONNECTION_TIMEOUT = 10_000L;
+  private static final long DEFAULT_READ_TIMEOUT = 10_000L;
+  private static final long DEFAULT_WRITE_TIMEOUT = 10_000L;
+
+  private final Retrofit retrofit;
+
+  private Snyk(Config config) {
+    if (config.token == null || config.token.isEmpty()) {
+      throw new IllegalArgumentException("Snyk API token is empty");
+    }
+
+    OkHttpClient.Builder builder = new OkHttpClient.Builder().connectTimeout(DEFAULT_CONNECTION_TIMEOUT, MILLISECONDS)
+                                                             .readTimeout(DEFAULT_READ_TIMEOUT, MILLISECONDS)
+                                                             .writeTimeout(DEFAULT_WRITE_TIMEOUT, MILLISECONDS);
+    builder.addInterceptor(new ServiceInterceptor(config.token, config.userAgent));
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    retrofit = new Retrofit.Builder().client(builder.build())
+                                     .baseUrl(config.baseUrl)
+                                     .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+                                     .build();
+  }
+
+  public static Snyk newBuilder(Config config) {
+    return new Snyk(config);
+  }
+
+  public SnykClient buildSync() {
+    return retrofit.create(SnykClient.class);
+  }
+
+  public static final class Config {
+    String baseUrl;
+    String token;
+    String userAgent;
+
+    public Config(String token) {
+      this.baseUrl = DEFAULT_BASE_URL;
+      this.token = token;
+      this.userAgent = DEFAULT_USER_AGENT;
+    }
+
+    public Config(String baseUrl, String token) {
+      this.baseUrl = baseUrl;
+      this.token = token;
+      this.userAgent = DEFAULT_USER_AGENT;
+    }
+  }
+}
---
+++ snyk-sdk/src/main/java/io/snyk/sdk/interceptor/ServiceInterceptor.java
@@ -0,0 +1,29 @@
+package io.snyk.sdk.interceptor;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ServiceInterceptor implements Interceptor {
+
+  private final String token;
+  private final String userAgent;
+
+  public ServiceInterceptor(String token, String userAgent) {
+    this.token = token;
+    this.userAgent = userAgent;
+  }
+
+  @Override
+  public Response intercept(Chain chain) throws IOException {
+    Request.Builder builder = chain.request().newBuilder();
+
+    builder.addHeader("Accept", "application/json")
+           .addHeader("Authorization", "token " + token)
+           .addHeader("User-Agent", userAgent);
+
+    return chain.proceed(builder.build());
+  }
+}

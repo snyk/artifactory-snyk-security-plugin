889aa1b01cc744a79855f88d43b2bbc72efce22c
pavel <36902936+pavel-github@users.noreply.github.com>
2019-05-24 16:58:54 +0200

    Add 'snyk-sdk-tests' module

---
+++ snyk-sdk-tests/src/test/java/io/snyk/sdk/SnykClientTest.java
@@ -0,0 +1,33 @@
+package io.snyk.sdk;
+
+import io.snyk.sdk.Snyk.Config;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SnykClientTest {
+
+  @Test
+  void createSnykClient_shouldThrowIAE_ifTokenIsNull() {
+    Config config = new Config(null);
+
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> Snyk.newBuilder(config).buildSync());
+    assertEquals("Snyk API token is empty", exception.getMessage());
+  }
+
+  @Test
+  void createSnykConfig_shouldReturnDefaultValues_ifNotDefined() {
+    Config config = new Config("snyk-api-token");
+
+    assertEquals("https://snyk.io/api/v1/", config.baseUrl);
+    assertEquals("snyk-sdk-java", config.userAgent);
+  }
+
+  @Test
+  void createSnykConfig_shouldReturnCorrectBaseUrl_IfOverridden() {
+    Config config = new Config("https://snyk-on-prem.local", "snyk-api-token");
+
+    assertEquals("https://snyk-on-prem.local", config.baseUrl);
+  }
+}

64e9b8f91b01cea25a8618ed1bdd1eb73b81c6c6
pavel <36902936+pavel-github@users.noreply.github.com>
2019-07-08 14:36:27 +0200

    Unify plugin configuration with default values

---
+++ core/src/main/java/io/snyk/plugins/artifactory/configuration/PluginConfiguration.java
@@ -0,0 +1,30 @@
+package io.snyk.plugins.artifactory.configuration;
+
+public enum PluginConfiguration implements Configuration {
+  // general settings
+  API_URL("snyk.api.url", "https://snyk.io/api/v1/"),
+  API_TOKEN("snyk.api.token", ""),
+  API_ORGANIZATION("snyk.api.organization", ""),
+
+  // scanner module
+  SCANNER_VULNERABILITY_THRESHOLD("snyk.scanner.vulnerability.threshold", "low"),
+  SCANNER_LICENSE_THRESHOLD("snyk.scanner.license.threshold", "low");
+
+  private final String propertyKey;
+  private final String defaultValue;
+
+  PluginConfiguration(String propertyKey, String defaultValue) {
+    this.propertyKey = propertyKey;
+    this.defaultValue = defaultValue;
+  }
+
+  @Override
+  public String propertyKey() {
+    return propertyKey;
+  }
+
+  @Override
+  public String defaultValue() {
+    return defaultValue;
+  }
+}
---
+++ core/src/test/java/io/snyk/plugins/artifactory/configuration/PluginConfigurationTest.java
@@ -0,0 +1,35 @@
+package io.snyk.plugins.artifactory.configuration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.snyk.plugins.artifactory.configuration.PluginConfiguration.API_ORGANIZATION;
+import static io.snyk.plugins.artifactory.configuration.PluginConfiguration.API_TOKEN;
+import static io.snyk.plugins.artifactory.configuration.PluginConfiguration.API_URL;
+import static io.snyk.plugins.artifactory.configuration.PluginConfiguration.SCANNER_LICENSE_THRESHOLD;
+import static io.snyk.plugins.artifactory.configuration.PluginConfiguration.SCANNER_VULNERABILITY_THRESHOLD;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("PluginConfiguration")
+class PluginConfigurationTest {
+
+  @DisplayName("check default values")
+  @Test
+  void checkDefaultValues() {
+    assertAll("should be not empty",
+              () -> assertEquals("https://snyk.io/api/v1/", API_URL.defaultValue(), getAssertionMessage(API_URL, "default value must be 'https://snyk.io/api/v1/'")),
+              () -> assertEquals("low", SCANNER_VULNERABILITY_THRESHOLD.defaultValue(), getAssertionMessage(SCANNER_VULNERABILITY_THRESHOLD, "default value must be 'low'")),
+              () -> assertEquals("low", SCANNER_LICENSE_THRESHOLD.defaultValue(), getAssertionMessage(SCANNER_LICENSE_THRESHOLD, "default value must be 'low'"))
+    );
+
+    assertAll("should be empty",
+              () -> assertEquals("", API_TOKEN.defaultValue(), getAssertionMessage(API_TOKEN, "default value must be empty")),
+              () -> assertEquals("", API_ORGANIZATION.defaultValue(), getAssertionMessage(API_ORGANIZATION, "default value must be empty"))
+    );
+  }
+
+  private String getAssertionMessage(PluginConfiguration entry, String message) {
+    return String.format("'%s' %s", entry.propertyKey(), message);
+  }
+}

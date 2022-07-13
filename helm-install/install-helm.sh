da8520a837487f7d5f8e095a6306724b10effc01
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-07-13 12:05:53 -0500

    feat: fix installation of snyk plugin

--- helm-install/install-helm.sh
+++ helm-install/install-helm.sh
@@ -1,18 +1,20 @@
-#!/bin/zsh
+#!/bin/bash
 # follows https://www.jfrog.com/confluence/display/JFROG/Installing+Artifactory#InstallingArtifactory-HelmInstallation
 set -e
-SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-minikube stop && minikube start --kubernetes-version=1.24.1 --disk-size=100g && eval "$(minikube docker-env)" || echo 'maybe u need to "brew install minikube"'
-minikube addons enable ingress
+if [[ $(minikube status -o json|jq -r '.Host') != "Running" ]]; then
+  minikube start --kubernetes-version=1.24.1 --disk-size=100g && eval "$(minikube docker-env)" || echo 'maybe u need to "brew install minikube"'
+  minikube addons enable ingress
+fi
 
-pushd "$SCRIPT_RELATIVE_DIR"/.. || exit 1
+pushd "$SCRIPT_DIR/.." || exit 1
   curl -s "https://get.sdkman.io" | bash
   source "$HOME/.sdkman/bin/sdkman-init.sh"
   sdk install java 11.0.15-zulu # please install sdkman for this to work
   sdk use java 11.0.15-zulu # please install sdkman for this to work
   # build
-  ./mvnw clean package -DskipTests
+  ./mvnw clean package -q -DskipTests
   # unzip distribution
   pushd distribution/target
     unzip artifactory-snyk-security-plugin-LOCAL-SNAPSHOT.zip
@@ -38,25 +40,31 @@ pushd "$SCRIPT_RELATIVE_DIR"/.. || exit 1
       rm snykSecurityPlugin.properties.temp
       mv snykSecurityPlugin.properties.temp2 snykSecurityPlugin.properties
     popd
+    minikube mount plugins:/tmp/artifactory/plugins &
   popd
-  # add plugin to kubernetes as configmap
+
+  if [[ $(helm list --namespace artifactory | grep -v NAMESPACE) != "" ]]; then
     helm uninstall --namespace artifactory artifactory
+  fi
+
   if [[ $(kubectl get ns | grep artifactory) != "" ]]; then
-        # remove previous deployment completely
       kubectl delete ns artifactory
   fi
+
   kubectl create ns artifactory
-  kubectl create cm snyk-plugin --from-file=distribution/target/plugins -n artifactory
 
   helm repo add jfrog https://charts.jfrog.io || echo "Helm repo already added"
   helm repo update
-  # Create a key
-  MASTER_KEY=$(openssl rand -hex 32)
+
+  # Create master key
+#  MASTER_KEY=$(openssl rand -hex 32)
+  MASTER_KEY="EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
   echo "Master Key: ${MASTER_KEY}"
   kubectl create secret generic my-masterkey-secret -n artifactory --from-literal=master-key="${MASTER_KEY}"
 
   # create join key
-  JOIN_KEY=$(openssl rand -hex 32)
+#  JOIN_KEY=$(openssl rand -hex 32)
+  JOIN_KEY="EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
   echo "Join Key: ${JOIN_KEY}"
   kubectl create secret generic my-joinkey-secret -n artifactory --from-literal=join-key="${JOIN_KEY}"
 
@@ -70,3 +78,5 @@ pushd "$SCRIPT_RELATIVE_DIR"/.. || exit 1
 popd || exit 1
 
 minikube service artifactory-artifactory-nginx -n artifactory
+sleep 10
+trap 'kill $(jobs -p)' EXIT

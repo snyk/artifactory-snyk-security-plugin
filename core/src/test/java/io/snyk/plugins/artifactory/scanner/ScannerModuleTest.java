0c9269cc332a27195ab3362707f90040f7269c2d
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-11-30 13:56:13 +0100

    chore: seems urllib's gotten more vulns

--- core/src/test/java/io/snyk/plugins/artifactory/scanner/ScannerModuleTest.java
+++ core/src/test/java/io/snyk/plugins/artifactory/scanner/ScannerModuleTest.java
@@ -366,7 +366,7 @@ public class ScannerModuleTest {
     TestResult tr = testResultCaptor.getValue();
     assertFalse(tr.success);
     assertEquals(1, tr.dependencyCount);
-    assertEquals(3, tr.issues.vulnerabilities.size());
+    assertEquals(5, tr.issues.vulnerabilities.size());
     assertEquals("pip", tr.packageManager);
     assertEquals(testSetup.org, tr.organisation.id);
 

359a7c5915e887945a947a830e29ce88f6ad3466
samsnyk <samuel.dahlberg@snyk.io>
2026-04-03 09:26:11 -0400

    fix: Ensure remote only check happens everytime lastModifiedDate is checked

--- core/src/main/java/io/snyk/plugins/artifactory/scanner/ScannerModule.java
+++ core/src/main/java/io/snyk/plugins/artifactory/scanner/ScannerModule.java
@@ -121,18 +121,19 @@ public class ScannerModule {
     Ignores ignores = Ignores.read(new RepositoryArtifactProperties(repoPath, repositories));
     Instant lastModifiedDate = getLastModifiedDate(repoPath);
     
+    return new MonitoredArtifact(repoPath.toString(), testResult, ignores, lastModifiedDate);
+  }
+
+  private Instant getLastModifiedDate(RepoPath repoPath) {
     // Only apply lastModifiedDate to packages from remote repositories.
     if(lastModifiedDateRemoteOnly()) {
       LOG.debug("Last modified date applied to only remote repositories.");
       if (!isRemoteRepository(repoPath)) {
         LOG.debug("Repository provided is not a remote repository, skipping last modified date check.");
-        lastModifiedDate = null;
+        return null;
       }
     }
-    return new MonitoredArtifact(repoPath.toString(), testResult, ignores, lastModifiedDate);
-  }
-
-  private Instant getLastModifiedDate(RepoPath repoPath) {
+    
     try {
       ItemInfo itemInfo = repositories.getItemInfo(repoPath);
       if (itemInfo != null) {
